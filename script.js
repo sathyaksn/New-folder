3b489f47895eb1d9c60f8ce6110071e12bba443a
sathyanarayanan <vijaysathi2001@gmail.com>
2023-05-31 18:49:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+// let containerDiv=document.createElement('div');
+// containerDiv.setAttribute('class','container')/
+// containerDiv.classList.add('text-center')
+// // for(leti=1;i<=6;i++){
+// // let rowDiv=document.createElement('row')
+// // rowDiv.setAttribute('class','row')
+// // containerDiv.appendChild(rowDiv)
+
+// // }
+// let rowDiv1=document.createElement('div')
+// rowDiv.setAttribute('class','row')
+// let colDiv1=document.createElement('div')
+// colDiv1=document.setAttribute('class','col-12')
+// containerDiv.appendChild(rowDiv1)
+// let inputelement=document.createElement(' input')
+// inputelement.setAttribute('class','form-control')
+// colDiv1.append(inputelement)
+
+// let rowDiv2=document.createElement('div')
+// let colDiv2=document.createElement('div')
+// colDiv2=document.setAttribute('class','col')
+// rowDiv.setAttribute('class','row')
+// containerDiv.appendChild(rowDiv2)
+// let rowDiv3=document.createElement('div')
+// rowDiv.setAttribute('class','row')
+// let colDiv3=document.createElement('div')
+// colDiv3=document.setAttribute('class','col')
+// containerDiv.appendChild(rowDiv3)
+// let rowDiv4=document.createElement('div')
+// rowDiv.setAttribute('class','row')
+// let colDiv4=document.createElement('div')
+// colDiv4=document.setAttribute('class','col')
+// containerDiv.appendChild(rowDiv4)
+// let rowDiv5=document.createElement('row')
+// let colDiv5=document.createElement('div')
+// colDiv5=document.setAttribute('class','col')
+// rowDiv.setAttribute('class','row')
+// containerDiv.appendChild(rowDiv5)
+// let rowDiv6=document.createElement('div')
+// rowDiv.setAttribute('class','row')
+// let colDiv6=document.createElement('div')
+// colDiv6=document.setAttribute('class','col')
+// containerDiv.appendChild(rowDiv6)
+
+// document.body.appendChild(containerDiv)
+
+let promise =new Promise((resolve, reject) => {
+setTimeout(()=>{
+resolve('success')
+},3000)
+})
+
+
